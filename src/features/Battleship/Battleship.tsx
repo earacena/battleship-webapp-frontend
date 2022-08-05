8206b942327359337c05695fc583c0b53f559fd1
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-08-05 14:11:29 -0400

    Implement keeping track of occupied positions

--- src/features/Battleship/Battleship.tsx
+++ src/features/Battleship/Battleship.tsx
@@ -1,14 +1,26 @@
-import React, { useState, useMemo } from 'react';
-import { DndContext, KeyboardSensor, MouseSensor, TouchSensor, useSensor, useSensors } from '@dnd-kit/core';
-import { createSnapModifier } from '@dnd-kit/modifiers';
-import { Board, Cell, CellProps, DraggablePiece, PieceProps } from './components';
-import { DragEndEvent, DragStartEvent } from '@dnd-kit/core/dist/types';
+import React, { useState } from "react";
+import {
+  DndContext,
+  KeyboardSensor,
+  MouseSensor,
+  TouchSensor,
+  useSensor,
+  useSensors,
+} from "@dnd-kit/core";
+import {
+  Board,
+  Cell,
+  CellProps,
+  DraggablePiece,
+  PieceProps,
+} from "./components";
+import { DragEndEvent, DragStartEvent } from "@dnd-kit/core/dist/types";
 
 const generateBoard = (boardSize: number): CellProps[][] => {
   // Create multidimensional array
   const board: CellProps[][] = Array.from(
     Array(boardSize),
-    () => new Array(boardSize),
+    () => new Array(boardSize)
   );
 
   // Populate array
@@ -17,69 +29,124 @@ const generateBoard = (boardSize: number): CellProps[][] => {
       board[y][x] = {
         id: `${y}-${x}`,
         x,
-        y
-      }
+        y,
+        occupied: false,
+      };
     }
   }
 
   return board;
-}
+};
 
-const generatePieces = (boardSize: number, gridSize: number): (PieceProps | undefined)[][] => {
+const generatePieces = (
+  boardSize: number,
+  gridSize: number,
+  setOccupiedPositions: (value: React.SetStateAction<boolean[][]>) => void,
+): (PieceProps | undefined)[][] => {
   const pieces: (PieceProps | undefined)[][] = Array.from(
+    Array(boardSize),
+    () => new Array(boardSize)
+  );
+
+  for (let y = 0; y < boardSize; ++y) {
+    for (let x = 0; x < boardSize; ++x) {
+      if (y === 0 && x >= 0 && x < 5) {
+        pieces[y][x] = {
+          id: `p-${y}-${x}`,
+          size: 0,
+          type: "",
+          position: { x, y },
+          vertical: true,
+          gridSize,
+        };
+      }
+    }
+  }
+  if (pieces[0][0]) {
+    pieces[0][0] = { ...pieces[0][0], size: 5, type: "carrier" };
+
+    for (let i = 0; i < pieces[0][0].size; ++i) {
+      setOccupiedPositions((prevPositions) => {
+        prevPositions[i][0] = true;
+        return prevPositions;
+      });
+    }
+  }
+  if (pieces[0][1]) {
+    pieces[0][1] = { ...pieces[0][1], size: 4, type: "battleship" };
+    
+    for (let i = 0; i < pieces[0][1].size; ++i) {
+      setOccupiedPositions((prevPositions) => {
+        prevPositions[i][1] = true;
+        return prevPositions;
+      });
+    }
+  }
+  if (pieces[0][2]) {
+    pieces[0][2] = { ...pieces[0][2], size: 3, type: "cruiser" };
+    
+    for (let i = 0; i < pieces[0][2].size; ++i) {
+      setOccupiedPositions((prevPositions) => {
+        prevPositions[i][2] = true;
+        return prevPositions;
+      });
+    }
+  }
+  if (pieces[0][3]) {
+    pieces[0][3] = { ...pieces[0][3], size: 3, type: "submarine" };
+    
+    for (let i = 0; i < pieces[0][3].size; ++i) {
+      setOccupiedPositions((prevPositions) => {
+        prevPositions[i][3] = true;
+        return prevPositions;
+      });
+    }
+  }
+  if (pieces[0][4]) {
+    pieces[0][4] = { ...pieces[0][4], size: 2, type: "destroyer" };
+
+    for (let i = 0; i < pieces[0][4].size; ++i) {
+      setOccupiedPositions((prevPositions) => {
+        prevPositions[i][4] = true;
+        return prevPositions;
+      });
+    }
+  }
+
+  return pieces;
+};
+
+const generateOccupiedPositions = (boardSize: number): boolean[][] => {
+  const occupiedPositions: boolean[][] = Array.from(
     Array(boardSize),
     () => new Array(boardSize),
   );
 
   for (let y = 0; y < boardSize; ++y) {
     for (let x = 0; x < boardSize; ++x) {
-      if (y === 0 && x >= 0 && x < 5) {
-        pieces[y][x] = { 
-          id: `p-${y}-${x}`,
-          size: 0,
-          type: '',
-          position: { x, y },
-          vertical: true,
-          gridSize
-        };
-      }
+      occupiedPositions[y][x] = false;
     }
   }
-  if (pieces[0][0]) {
-    pieces[0][0] = { ...pieces[0][0], size: 5, type: 'carrier' };
-  }
-  if (pieces[0][1]) {
-    pieces[0][1] = { ...pieces[0][1], size: 4, type: 'battleship' };
-  }
-  if (pieces[0][2]) {
-    pieces[0][2] = { ...pieces[0][2], size: 3, type: 'cruiser' };
-  }
-  if (pieces[0][3]) {
-    pieces[0][3] = { ...pieces[0][3], size: 3, type: 'submarine' };
-  }
-  if (pieces[0][4]) {
-    pieces[0][4] = { ...pieces[0][4], size: 2, type: 'destroyer' };
-  }
 
-  return pieces;
+  return occupiedPositions;
 };
 
 function Battleship() {
-  
   const boardSize: number = 10;
   const gridSize: number = 50;
 
   const [board] = useState(() => generateBoard(boardSize));
-  const [pieces, setPieces] = useState(() => generatePieces(boardSize, gridSize));
+  const [occupiedPositions, setOccupiedPositions] = useState<boolean[][]>(() => 
+    generateOccupiedPositions(boardSize)
+  );
+  const [pieces, setPieces] = useState(() =>
+    generatePieces(boardSize, gridSize, setOccupiedPositions)
+  );
   const [movingPiece, setMovingPiece] = useState<PieceProps | null>(null);
   const mouseSensor = useSensor(MouseSensor, {});
   const touchSensor = useSensor(TouchSensor, {});
   const keyboardSensor = useSensor(KeyboardSensor, {});
-  const sensors = useSensors(
-    mouseSensor,
-    touchSensor,
-    keyboardSensor,
-  );
+  const sensors = useSensors(mouseSensor, touchSensor, keyboardSensor);
 
   const handleOnDragStart = ({ active }: DragStartEvent) => {
     const piece = pieces.reduce<PieceProps | undefined>((acc, row) => {
@@ -96,9 +163,9 @@ function Battleship() {
       console.log(movingPiece, event);
       return;
     }
-    console.log('update');
+    console.log("update");
     const { x: movingPieceX, y: movingPieceY } = movingPiece.position;
-    const [cellY, cellX] = event.over.id.toString().split('-').map(Number);
+    const [cellY, cellX] = event.over.id.toString().split("-").map(Number);
     console.log(`[${cellY}, ${cellX}]`);
     const possiblyExistingPiece = pieces[cellY][cellX];
     setMovingPiece(null);
@@ -106,7 +173,7 @@ function Battleship() {
     if (event.over && !possiblyExistingPiece) {
       const newPiece: PieceProps = {
         ...movingPiece,
-        position: { x: cellX, y: cellY }
+        position: { x: cellX, y: cellY },
       };
 
       const newPieces = pieces.map((row) => row.slice());
@@ -123,27 +190,30 @@ function Battleship() {
   };
 
   return (
-    <DndContext 
+    <DndContext
       sensors={sensors}
       onDragStart={handleOnDragStart}
       onDragCancel={handleOnDragCancel}
       onDragEnd={handleOnDragEnd}
     >
       <Board size={boardSize} gridSize={gridSize}>
-        {board.map((row, y) => 
+        {board.map((row, y) =>
           row.map((cell, x) => {
+            const occupied = occupiedPositions[y][x];
             if (pieces[y][x]) {
               const piece = pieces ? pieces[y][x] : null;
               if (piece) {
+
                 return (
-                  <Cell key={cell.id} {...cell}>
+                  <Cell key={cell.id} {...cell} occupied={true}>
                     <DraggablePiece key={piece.id} {...piece} />
                   </Cell>
                 );
               }
             }
-            return <Cell key={cell.id} {...cell} />;
-        }))}
+            return <Cell key={cell.id} {...cell} occupied={occupied}/>;
+          })
+        )}
       </Board>
     </DndContext>
   );
