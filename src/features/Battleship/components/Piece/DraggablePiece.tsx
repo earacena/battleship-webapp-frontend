9393b395997d4fddf1372c963eee16cb648b468f
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-08-06 02:24:10 -0400

    Add drag indicator icon and styles for Piece

--- src/features/Battleship/components/Piece/DraggablePiece.tsx
+++ src/features/Battleship/components/Piece/DraggablePiece.tsx
@@ -2,8 +2,9 @@ import React from 'react';
 import { useDraggable } from '@dnd-kit/core';
 import { CSS } from '@dnd-kit/utilities';
 import Piece, { PieceProps } from './Piece';
-import { RiShip2Fill } from 'react-icons/ri';
-
+import { GrDrag } from 'react-icons/gr';
+import styles from './Piece.module.css';
+import { Label } from './components/';
 
 function DraggablePiece(props: PieceProps) {
   const { attributes, listeners, setNodeRef, transform } = useDraggable({
@@ -17,12 +18,14 @@ function DraggablePiece(props: PieceProps) {
   return (
     <Piece
       ref={setNodeRef}
+      className={styles.Piece}
       style={style}
       {...props}
       {...listeners}
       {...attributes}
     >
-      <RiShip2Fill />
+      <GrDrag />
+      <Label vertical={props.vertical} content={props.type} />
     </Piece>
   )
 }
