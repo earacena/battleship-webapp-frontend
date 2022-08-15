20ad13acf4295ec6b0d6fa159c6befd2b8ba8c26
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-08-15 02:05:21 -0400

    Add title header to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import { TitleHeader } from './components';
 import { Battleship } from './features/Battleship';
 
 function App() {
-
   return (
     <div className="App">
+      <TitleHeader title="BATTLESHIP" />
       <Battleship />
     </div>
   );
