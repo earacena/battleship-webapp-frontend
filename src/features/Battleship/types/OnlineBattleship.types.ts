08ef05d56bf33a74735ef8dbd657a61ce6fb92b3
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-08-19 21:55:22 -0400

    Add IdMessage parser and type

--- src/features/Battleship/types/OnlineBattleship.types.ts
+++ src/features/Battleship/types/OnlineBattleship.types.ts
@@ -2,9 +2,15 @@ import { z } from 'zod';
 
 export const zString = z.string();
 
+export const IdMessage = z.object({
+  type: z.string(),
+  message: z.string(),
+});
+
 export const Message = z.object({
   type: z.string(),
   message: z.string(),
 });
 
+export type IdMessageType = z.infer<typeof IdMessage>;
 export type MessageType = z.infer<typeof Message>;
