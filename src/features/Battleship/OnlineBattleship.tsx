eda34db111c5238889b0446531bfc276f27bf814
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-08-22 01:33:28 -0400

    Implement sending an enqueue event to server

--- src/features/Battleship/OnlineBattleship.tsx
+++ src/features/Battleship/OnlineBattleship.tsx
@@ -1,15 +1,15 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import { IdMessage, Message, zString } from './types/OnlineBattleship.types';
 import type { IdMessageType, MessageType} from './types/OnlineBattleship.types';
 
 function OnlineBattleship() {
+  const [isQueuing, setIsQueuing] = useState<boolean>(false);
   const ws = useRef<WebSocket>();
 
   useEffect(() => {
     ws.current = new WebSocket('ws://localhost:8080');
     ws.current.onopen = () => {
       console.log('connected to websocket server');
-      ws.current?.send('hi from client');
     };
     ws.current.onclose = () => {
       console.log('closed connection to websocket server');
@@ -42,6 +42,9 @@ function OnlineBattleship() {
             const parsedMessage: MessageType = Message.parse(message);
             console.log(parsedMessage);
             break;
+          case 'queued user':
+            setIsQueuing(true);
+            break;
         };
       } else {
         console.error(`malformatted websocket message received: ${message}`)
@@ -49,9 +52,20 @@ function OnlineBattleship() {
     }
   }, []);
 
+  const handleEnqueue = () => {
+    if (!ws.current) {
+      return;
+    }
+
+    const messageJSON: string = JSON.stringify({ type: 'enqueue user' });
+    ws.current?.send(messageJSON);
+  };
+
   return (
     <div>
-      
+      {!isQueuing && "Currently not waiting in a queue"}
+      {isQueuing && "Waiting in a queue..."}
+      <button onClick={handleEnqueue}>Queue for match</button>
     </div>
   );
 }
