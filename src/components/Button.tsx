c5885171af5be4d79aaf0ff2304f1a7fee360423
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-08-29 21:31:12 -0400

    Implement and re-export reusable button component

---
+++ src/components/Button.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+type ButtonProps = {
+  children?: React.ReactNode,
+};
+
+function Button({ children }: ButtonProps) {
+  return (
+    <button>
+      {children}
+    </button>
+  );
+}
+
+export default Button;
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,2 @@
 export { default as TitleHeader } from './TitleHeader';
+export { default as Button } from './Button';
