45d844a97f28d6f9c0c5d45bceb83386e3a75f36
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-08-29 22:00:14 -0400

    Add base style to Button and additional props

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,12 +1,30 @@
-import React from 'react';
+import React from "react";
 
 type ButtonProps = {
-  children?: React.ReactNode,
+  onClick: () => void;
+  disabled?: boolean,
+  children?: React.ReactNode;
 };
 
-function Button({ children }: ButtonProps) {
+function Button({ onClick, disabled, children }: ButtonProps) {
   return (
-    <button>
+    <button
+      style={
+        {
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          margin: "10px",
+          fontSize: "30px",
+          border: "none",
+          borderRadius: "5px",
+          cursor: 'pointer',
+        } as React.CSSProperties
+      }
+      onClick={onClick}
+      disabled={disabled}
+    >
       {children}
     </button>
   );
