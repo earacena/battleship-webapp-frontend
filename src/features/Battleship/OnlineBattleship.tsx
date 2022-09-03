483721ba78ffc016f263a1f02235e0f42e2078b3
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-09-02 23:37:16 -0400

    Modify accessing .env

--- src/features/Battleship/OnlineBattleship.tsx
+++ src/features/Battleship/OnlineBattleship.tsx
@@ -73,7 +73,7 @@ function OnlineBattleship() {
   const ws = useRef<WebSocket>();
 
   useEffect(() => {
-    ws.current = new WebSocket(zString.parse(process.env['BACKEND_URL']));
+    ws.current = new WebSocket(zString.parse(process.env.REACT_APP_BACKEND_URL));
     ws.current.onopen = () => {
       console.log("connected to websocket server");
       setIsConnected(true);
