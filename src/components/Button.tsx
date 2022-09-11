3d1398deda4d6a7a7021a5dcd0a58b37ea97b728
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-09-10 23:05:04 -0400

    Add optional style prop to Button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,18 +4,21 @@ type ButtonProps = {
   onClick: () => void;
   disabled?: boolean,
   children?: React.ReactNode;
+  style?: React.CSSProperties;
 };
 
-function Button({ onClick, disabled, children }: ButtonProps) {
+function Button({ onClick, disabled, children, style }: ButtonProps) {
   return (
     <button
       style={
         {
+          ...style,
           display: "flex",
           flexDirection: "column",
           justifyContent: "center",
           alignItems: "center",
           margin: "10px",
+          marginTop: "20px",
           fontSize: "30px",
           border: "none",
           borderRadius: "5px",
