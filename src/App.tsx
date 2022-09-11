863a33c6571f7399a94455136501c2bfa926ef3f
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-09-10 23:10:21 -0400

    Refactor App with Button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,34 @@
-import React from 'react';
-import { Routes, Route, useNavigate } from 'react-router-dom';
-import { TitleHeader } from './components';
-import { Battleship, OnlineBattleship } from './features/Battleship';
-import { Menu } from './features/Menu';
+import React from "react";
+import { Routes, Route, useNavigate } from "react-router-dom";
+import { TitleHeader } from "./components";
+import { Battleship, OnlineBattleship } from "./features/Battleship";
+import { Menu } from "./features/Menu";
+import { Button } from "./components";
 
 function App() {
   const navigate = useNavigate();
 
   return (
-    <div className="App" style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' } as React.CSSProperties}>
-      <button style={{ border: 'none', cursor: 'pointer' } as React.CSSProperties} onClick={() => navigate("/")}>
+    <div
+      className="App"
+      style={
+        {
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+        } as React.CSSProperties
+      }
+    >
+      <Button
+        style={{ border: "none", cursor: "pointer" } as React.CSSProperties}
+        onClick={() => navigate("/")}
+      >
         <TitleHeader title="BATTLESHIP" />
-      </button>
+      </Button>
       <Routes>
-        <Route path="/"  element={<Menu />} />
-        <Route path="solo" element={<Battleship /> } />
+        <Route path="/" element={<Menu />} />
+        <Route path="solo" element={<Battleship />} />
         <Route path="online" element={<OnlineBattleship />} />
       </Routes>
     </div>
