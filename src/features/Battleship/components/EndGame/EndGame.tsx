bd6c0e002393c6a28961ccfcc982415188d448de
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-09-14 22:47:51 -0400

    Add fade-in transition for EndGame component

--- src/features/Battleship/components/EndGame/EndGame.tsx
+++ src/features/Battleship/components/EndGame/EndGame.tsx
@@ -1,21 +1,33 @@
-import React from 'react';
+import React from "react";
+import styles from '../../../../app.module.css';
 
 type EndGameProps = {
-  winner: string,
-  loser: string,
-  gameResult: string,
-  resetGame: () => void,
+  winner: string;
+  loser: string;
+  gameResult: string;
+  resetGame: () => void;
 };
 
 function EndGame({ winner, loser, gameResult, resetGame }: EndGameProps) {
   return (
-    <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
-      <span style={{ fontSize: '40px'}}>
-        <span style={{ color: 'green' }}>{winner}</span> has sunk all of <span style={{ color: 'red' }}>{loser}</span>'s ships!
+    <div
+      className={styles.FadeInComponentSlowly}
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <span style={{ fontSize: "40px" }}>
+        <span style={{ color: "green" }}>{winner}</span> has sunk all of{" "}
+        <span style={{ color: "red" }}>{loser}</span>'s ships!
       </span>
-      <button style={{fontSize: '30px'}} onClick={resetGame}>Play Again</button>
+      <button style={{ fontSize: "30px" }} onClick={resetGame}>
+        Play Again
+      </button>
     </div>
   );
-};
+}
 
 export default EndGame;
