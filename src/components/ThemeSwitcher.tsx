ce5d788d7856d571fe4f843369cba29990e589e1
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-09-15 22:57:58 -0400

    Implement ThemeSwitcher component

---
+++ src/components/ThemeSwitcher.tsx
@@ -0,0 +1,29 @@
+import React, { useContext } from 'react';
+import { BsSun, BsMoonFill } from 'react-icons/bs';
+import { ThemeContext } from '../App';
+import Button from './Button';
+
+function ThemeSwitcher() {
+  const { theme, setTheme } = useContext(ThemeContext);
+
+  return(
+    <Button
+      style={{
+        borderRadius: "50%",
+        padding: "5px",
+      } as React.CSSProperties}
+      onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}
+    >
+      {
+        theme === 'light'
+        ? (
+            <BsMoonFill />
+            ) : (
+            <BsSun />
+          )
+      }
+    </Button>
+  );
+}
+
+export default ThemeSwitcher;
