28e5d86b81f15b2d4f5388961631a67316b26098
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-09-15 22:57:34 -0400

    Implement Header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import Button from './Button';
+import TitleHeader from './TitleHeader';
+import ThemeSwitcher from './ThemeSwitcher';
+
+function Header() {
+  const navigate = useNavigate();
+
+  return (
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+      } as React.CSSProperties}
+    >
+      <Button
+        style={{
+          border: "none",
+          cursor: "pointer",
+        } as React.CSSProperties}
+
+        onClick={() => navigate("/")}
+      >
+        <TitleHeader title="BATTLESHIP" />
+      </Button>
+      <ThemeSwitcher />
+    </div>
+  );
+}
+
+export default Header;
