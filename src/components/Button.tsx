f911189975818b1f749bb9376c8e6a9ccd8c8140
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-09-15 22:55:44 -0400

    Implement default style that can be overwritten

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -8,21 +8,26 @@ type ButtonProps = {
 };
 
 function Button({ onClick, disabled, children, style }: ButtonProps) {
+
+  const defaultStyle: React.CSSProperties = {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    margin: "10px",
+    marginTop: "20px",
+    fontSize: "30px",
+    border: "none",
+    borderRadius: "5px",
+    cursor: 'pointer',
+  }
+
   return (
     <button
       style={
         {
+          ...defaultStyle,
           ...style,
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-          alignItems: "center",
-          margin: "10px",
-          marginTop: "20px",
-          fontSize: "30px",
-          border: "none",
-          borderRadius: "5px",
-          cursor: 'pointer',
         } as React.CSSProperties
       }
       onClick={onClick}
