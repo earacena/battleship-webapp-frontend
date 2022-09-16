6e790ef21c84dc0d1d628c79f26d67f1a3643a3b
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-09-15 22:56:57 -0400

    Implement ThemeContext for themeing

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,48 @@
-import React from "react";
+import React, { createContext, Dispatch, useState } from "react";
 import { Routes, Route, useNavigate } from "react-router-dom";
-import { TitleHeader } from "./components";
 import { Battleship, OnlineBattleship } from "./features/Battleship";
 import { Menu } from "./features/Menu";
 import { Button } from "./components";
+import { Header } from "./components";
 
 import styles from './app.module.css';
+import classNames from "classnames";
+
+type ThemeContextProps = {
+  theme: string;
+  setTheme: Dispatch<string>;
+}
+
+export const ThemeContext = createContext<ThemeContextProps>({ theme: '', setTheme: () => null });
 
 function App() {
-  const navigate = useNavigate();
+  const [theme, setTheme] = useState<string>('light');
 
   return (
-    <div
-      className={styles.FadeInComponentSlowly}
-      style={
-        {
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-          alignItems: "center",
-        } as React.CSSProperties
-      }
-    >
-      <Button
-        style={{ border: "none", cursor: "pointer" } as React.CSSProperties}
-        onClick={() => navigate("/")}
+    <ThemeContext.Provider value={{ theme, setTheme }}>
+      <Header />
+      <div
+        className={classNames(
+          styles.App,
+          styles.FadeInComponentSlowly
+        )}
+        style={
+          {
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            "--background-color": theme === 'light' ? 'white' : 'darkslategray',
+          } as React.CSSProperties
+        }
       >
-        <TitleHeader title="BATTLESHIP" />
-      </Button>
-      <Routes>
-        <Route path="/" element={<Menu />} />
-        <Route path="solo" element={<Battleship />} />
-        <Route path="online" element={<OnlineBattleship />} />
-      </Routes>
-    </div>
+        <Routes>
+          <Route path="/" element={<Menu />} />
+          <Route path="solo" element={<Battleship />} />
+          <Route path="online" element={<OnlineBattleship />} />
+        </Routes>
+      </div>
+    </ThemeContext.Provider>
   );
 }
 
