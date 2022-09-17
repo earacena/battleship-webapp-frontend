740c3c433889327c23eccb7d10f3eff2fa0d5924
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-09-16 23:50:59 -0400

    Update Button path

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import Button from './Button';
+import { Button } from './Button';
 import TitleHeader from './TitleHeader';
 import ThemeSwitcher from './ThemeSwitcher';
 
