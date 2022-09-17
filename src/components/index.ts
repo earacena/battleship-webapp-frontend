44095e97a964b039b0c1c7d76eb25ee8dbffc233
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-09-16 23:49:55 -0400

    Update Button path

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,3 @@
 export { default as TitleHeader } from './TitleHeader';
-export { default as Button } from './Button';
+export { Button } from './Button';
 export { default as Header } from './Header';
